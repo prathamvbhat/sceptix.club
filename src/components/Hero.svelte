c4fcc08cad77bf6e815473a1de5a6db1347bf6aa
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-04-08 21:36:28 +0530

    Fonts ans customisation

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -13,13 +13,13 @@
 
         document.onmousemove = e => handleOnMove(e);
     });
-
 </script>
 
 <main class="h-screen overflow-hidden relative">
+
     <div class="h-screen w-full overflow-hidden absolute z-20
     bg-stone-50 text-stone-950" id="left-box">
-        <div class="w-screen h-screen flex flex-col items-center justify-center">
+        <div class="w-screen h-screen flex flex-col items-center justify-center absolute z-10">
             <h1 class="font-bold text-7xl my-6">
                 The <span class="code bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-purple-400 ">
                     sceptix</span> club
@@ -29,9 +29,10 @@
             </h3>
         </div>
     </div>
+
     <div class="h-screen w-full overflow-hidden absolute z-10
     bg-stone-950 text-stone-50" id="right-box">
-        <div class="w-full h-screen flex flex-col items-center justify-center">
+        <div class="w-full h-screen flex flex-col items-center justify-center absolute z-10">
             <h1 class="font-bold text-7xl my-6">
                 The <span class="code bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-sky-400">
                     freedom</span> club
@@ -40,11 +41,31 @@
                 Unchain Your Will
             </h3>
         </div>
+        <div id="right-img" class="w-screen h-screen absolute z-0"></div>
     </div>
+    
 </main>
 
 <style lang="postcss">
     .code {
         font-weight: 700;
+    }
+
+    #right-img {
+        background-image: url('images/darkdrop.png');
+        background-size: 100vmin;
+        background-position: center;
+        background-repeat: no-repeat;
+        animation: whirl 31.4s linear infinite;
+    }
+
+    @keyframes whirl {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
     }
 </style>
--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,3 +1,4 @@
 <aside class="fixed w-16 h-screen bg-gradient-to-br from-sky-400 to-cyan-400 z-30">
-    <!-- <img class="p-2" src="/images/htf.png" alt="The sceptix club logo"> -->
+    <!-- <a href="/"><img class="px-1 py-4" src="/images/nav-icon.png" alt="The sceptix club logo">
+    </a> -->
 </aside>
